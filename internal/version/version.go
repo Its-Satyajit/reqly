693f90f3d2f9b0c9046cd3bdce7ee4e6373a4278
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2026-08-16 12:01:35 +0530

    chore(main): release 0.2.0

---
+++ internal/version/version.go
@@ -0,0 +1,24 @@
+// Reqly - A local-first, Git-native API development environment.
+// Copyright (C) 2026 It's Satyajit
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Package version holds the release version string for Reqly.
+package version
+
+// Version is the current release version. release-please bumps it on release
+// PRs via the x-release-please-version annotation.
+const Version = "0.2.0" // x-release-please-version
